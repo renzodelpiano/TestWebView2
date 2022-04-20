882af017530a9373f7bc61be809a747478a2b383
Renzo Del Piano <renzo.delpiano@micra.it>
2022-04-20 16:58:08 +0200

    first commit

---
+++ TestProject/TestProject/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+
+namespace TestProject {
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            InitializeWB();
+        }
+
+        private async Task InitializeWB() {
+            await wb.EnsureCoreWebView2Async();
+            wb.CoreWebView2.Navigate("file://" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "editor_summernote.html").Replace(@"\", @"/"));
+        }
+    }
+}
